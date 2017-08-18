62874fa9d1520e401734f5818f5a846d6de9a1de
jisoo <1234milejs@gmail.com>
2017-08-18 18:01:21 +0900

    추가

--- Helpet/HelpetTableViewController.swift
+++ Helpet/HelpetTableViewController.swift
@@ -7,45 +7,130 @@
 //
 
 import UIKit
+import Foundation
 
 class HelpetTableViewController: UITableViewController {
 
+    // 리스트에 뿌릴 배열선언과 생성
+    var pets:[LostPet] = Array()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let pet1 = LostPet(name: "진돌이",
+                           sex: "수컷",
+                           kind: "진도믹스",
+                           age: 1,
+                           place: "방배동 서울고등학교 사거리",
+                           rmk: "사람을 잘 따름, 귀 뒤 검은색 반점이 있음, 흰색, 진돗개",
+                           phone: 01012346541,
+                           petimage: UIImage(named:"dog1")!,
+                           lostdate: "2017-08-01" )
+        
+        let pet2 = LostPet(name: "호이",
+                           sex: "수컷",
+                           kind: "웰시코기",
+                           age: 1,
+                           place: "길음동 현대아파트 3단지",
+                           rmk: "한쪽다리를 절음(수술한 흔적)",
+                           phone: 01012346541,
+                           petimage: UIImage(named:"dog2")!,
+                           lostdate: "2017-08-03" )
+        
+        let pet3 = LostPet(name: "봉봉이",
+                           sex: "암컷",
+                           kind: "웰시코기",
+                           age: 3,
+                           place: "군자동 어린이대공원",
+                           rmk: "등에 갈색 털이 하트모양으로 나있음, 앞니가 빠져있음",
+                           phone: 01012346541,
+                           petimage: UIImage(named:"dog3")!,
+                           lostdate: "2017-07-13" )
+        
+        let pet4 = LostPet(name: "뚜이",
+                           sex: "수컷",
+                           kind: "말티즈",
+                           age: 4,
+                           place: "송파구 올림픽공원",
+                           rmk: "앞발톱은 검은색, 뒷발톱은 흰색임",
+                           phone: 01012346541,
+                           petimage: UIImage(named:"dog4")!,
+                           lostdate: "2017-08-17" )
+        
+        let pet5 = LostPet(name: "맥스",
+                           sex: "암컷",
+                           kind: "골든리트리버",
+                           age: 5,
+                           place: "후암동 용산고등학교 사거리",
+                           rmk: "꼬리가 귀보다 짧음, 건치미남, 치아가 많이있음",
+                           phone: 01012346541,
+                           petimage: UIImage(named:"dog5")!,
+                           lostdate: "2017-06-30" )
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+       
+        self.pets.append(pet1)
+        self.pets.append(pet2)
+        self.pets.append(pet3)
+        self.pets.append(pet4)
+        self.pets.append(pet5)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
-    // MARK: - Table view data source
-
+    
+    // 테이블뷰 섹션 갯수
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+       
+        return 1
     }
 
+    // 섹션의 셀수 (pets이 담고있는 아이템 수만큼 )
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return pets.count
     }
 
-    /*
+    // 셀 하나씩 만들때마다 타는 함수 (스토리보드 cell의 Identifier에 reuseIdentifier 추가)
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        if let petCell = cell as? HelpetTableViewCell{
+            
+            let pet = self.pets[indexPath.row]
+        
+            /*
+            let numFormatter:NumberFormatter = NumberFormatter()
+            numFormatter.numberStyle = NumberFormatter.Style.decimal
+            
+            
+            if let age = pet.age {
+                let ageStr = numFormatter.string(from: NSNumber(integerLiteral: age))
+                petCell.petAgeLabel.text = ageStr
+            } else {
+                petCell.petAgeLabel.text = ""
+            }
+            */
+            
+            petCell.petNameLabel.text = pet.name
+            petCell.petKindLabel.text = pet.kind
+            petCell.petSexLabel.text = pet.sex
+            petCell.petPlaceLabel.text = pet.place
+            petCell.petImageView.image = pet.petimage
+            petCell.petLostDateLabel.text = pet.lostdate
+      
+            return petCell
+        
+        }
+       
 
         return cell
     }
-    */
+   
 
     /*
     // Override to support conditional editing of the table view.
@@ -92,4 +177,37 @@ class HelpetTableViewController: UITableViewController {
     }
     */
 
+}
+
+
+
+class HelpetTableViewCell: UITableViewCell{
+
+    
+    @IBOutlet weak var petImageView: UIImageView!
+    
+    @IBOutlet weak var petNameLabel: UILabel!
+
+    
+    @IBOutlet weak var petSexLabel: UILabel!
+    
+    @IBOutlet weak var petKindLabel: UILabel!
+    
+    
+    @IBOutlet weak var petPlaceLabel: UILabel!
+    
+    @IBOutlet weak var petLostDateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    
+    }
+    
+    
 }
--- Helpet/LostPet.swift
+++ Helpet/LostPet.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import UIKit
+
+class LostPet{
+
+    var name:String  //이름
+    var sex:String    //성별
+    var kind:String?  //품종
+    var age:Int?     //나이
+    var place:String  // 잃어버린곳 (이건gps좌표로 변경필요)
+    var rmk:String?  //특징 및 비고
+    var phone:Int?  //전화번호
+    var petimage:UIImage?  //사진
+    var lostdate:String // 실종일자
+    
+    init(name:String,
+         sex:String,
+         kind:String?,
+         age:Int?,
+         place:String,
+         rmk:String,
+         phone:Int?,
+         petimage:UIImage?,
+        lostdate:String){
+    
+        self.name = name
+        self.sex = sex
+        self.kind = kind
+        self.age = age
+        self.place = place
+        self.rmk = rmk
+        self.phone = phone
+        self.petimage = petimage
+        self.lostdate = lostdate
+        
+    }
+    
+}
